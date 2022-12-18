ece9696942a6fa2d97c9d1fdeeead75245bf7ec7
Nnolum Eustace <eustacennolum@gmail.com>
2022-12-18 10:44:13 +0000

    committed changes on task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
- * function that checks for upper case
+ * _isupper - uppercase letters
+ * @c: character to check
  *
- * reeturn: Always 0.
+ * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
-	if(c >='A' && c <='Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
+
 }
